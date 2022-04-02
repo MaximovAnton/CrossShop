97f3f9f3973e1be62c2af6e919db88d51b52c623
anton <maximov.antonn@gmail.com>
2022-04-03 00:58:36 +0300

    d

--- crossshop/src/Components/Content/Content.jsx
+++ crossshop/src/Components/Content/Content.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import './Content.css'
-import Card from '../Card/Card'
+import React from "react";
+import "./Content.css";
+import Card from "../Card/Card";
 
 const Content = () => {
-    return(
-        <div className="content">
-            <Card />
-            <Card />
-            <Card />
+  return (
+    <div>
+        <div className="title">
+            <h3>Весь ассортимент</h3>
         </div>
-    )
-}
+      
+      <div className="content">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
 
-export default Content
+export default Content;
