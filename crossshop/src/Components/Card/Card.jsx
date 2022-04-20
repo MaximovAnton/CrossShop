eaeec3c57b683f0f14ea803d6f7c696e2279713b
anton <maximov.antonn@gmail.com>
2022-04-20 21:06:03 +0300

    card

--- crossshop/src/Components/Card/Card.jsx
+++ crossshop/src/Components/Card/Card.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import './Card.css'
+import svg_unliked from '../../images/heart_unliked.png'
+import like from '../../images/heart_like.svg'
+import plus from '../../images/plus.svg'
+
 
 const Card = () => {
     return(
             <div className="card">
-                Card
+                <div className="favorite">
+                    <img src={svg_unliked} alt="" width={30} height={30}/>
+                </div>
+                
+                <div className="card_img">
+                    <img src="https://zastavok.net/main/raznoe/1415018881.jpg" alt="" />
+                </div>
+                
+                    <div className="card_title">Nike</div>
+                    <div className="card_bottom">
+                        <div className="card_price">100$</div>
+                        <img src={plus} alt="" />
+                    </div>
+                    
+                
             </div>
     )
 }
